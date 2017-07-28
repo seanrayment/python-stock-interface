fc501bdba6416330e5865b746f5aa36d2a4e76e6
seanrayment <rayment.code@gmail.com>
2017-07-28 08:11:03 -0400

    command line interface

--- stocks.py
+++ stocks.py
@@ -2,12 +2,27 @@ import json
 import urllib.request
 from termcolor import colored
 
-response = urllib.request.urlopen('https://api.iextrading.com/1.0/stock/goog/quote')
-json_response = response.read()
-json_data = json.loads(json_response)
+shouldExit = False
 
-for key in json_data:
-    print (colored(str(key) + ": ", 'red'), colored(str(json_data[key]), 'blue'))
+while not shouldExit:
+
+    symbol = input('Enter a stock symbol (exit to quit): ')
+
+    if symbol == 'exit':
+        shouldExit = True
+
+    response = urllib.request.urlopen('https://api.iextrading.com/1.0/stock/'+symbol+'/quote')
+
+    json_response = response.read()
+    json_data = json.loads(json_response)
+
+    for key in json_data:
+        print(colored(str(key) + ": ", 'red'), colored(str(json_data[key]), 'blue'))
+
+
+print('PROGRAM EXITED')
+
+
 
 
 
