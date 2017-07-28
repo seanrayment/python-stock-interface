efdc15b718aa446b302a8c971b12885014987d62
seanrayment <rayment.code@gmail.com>
2017-07-27 21:53:20 -0400

    json decoding, beautifying the console

--- stocks.py
+++ stocks.py
@@ -1,6 +1,13 @@
 import json
 import urllib.request
+from termcolor import colored
 
-response = urllib.request.urlopen('https://api.iextrading.com/1.0/stock/aapl/quote')
+response = urllib.request.urlopen('https://api.iextrading.com/1.0/stock/goog/quote')
 json_response = response.read()
-print(json_response)
+json_data = json.loads(json_response)
+
+for key in json_data:
+    print (colored(str(key) + ": ", 'red'), colored(str(json_data[key]), 'blue'))
+
+
+
